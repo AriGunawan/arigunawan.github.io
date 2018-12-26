176992ec37df9aeb14c2e0e06da7950a853b12cd
Ari Gunawan <ari.gunawan@geekseat.com.au>
2018-12-26 21:47:10 +0700

    Implement hero swiper

---
+++ index.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  // spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  speed: 1000
+});
